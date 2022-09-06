4b531c65375c327e5b4118e36ac98b08dd9eb294
DanielSan <56778742+kaiserkey@users.noreply.github.com>
2022-09-06 17:30:31 -0300

    Add files via upload

---
+++ forma_abrebiada_de_php.php
@@ -0,0 +1,15 @@
+<?
+
+echo "<br> Estra es la forma abrebiada de php";
+echo "<br> Para poder usar esto es necesario activar la opcion de wamp (short_open_tag ON)";
+
+
+?>
+
+<?php
+
+echo "<br><br> Esta es la forma no abrebiada de php";
+echo "<br> Para poder usar esto es necesario activar la opcion de wamp (short_open_tag ON - OF)";
+echo "<br> Esta opcion viene por defecto para usar PHP";
+
+?>
