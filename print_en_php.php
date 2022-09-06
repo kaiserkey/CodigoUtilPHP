947eb7ae3b8c521999cebf96818dab4103138dd7
DanielSan <56778742+kaiserkey@users.noreply.github.com>
2022-09-06 17:33:10 -0300

    Add files via upload

---
+++ print_en_php.php
@@ -0,0 +1,12 @@
+<?
+
+/**
+ * Existen varias formas de mostrar informacion en pantalla en php pero las mas utilizadas
+ * son las de print y echo
+ */
+
+print "<br> Hello World";
+echo "<br> Hello World";
+
+
+?>
